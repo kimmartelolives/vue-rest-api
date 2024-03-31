caa6d1c234759d8df2cc6342c3958040813ec230
Kim Martel Olives <kimmartel.olives@gmail.com>
2024-03-31 16:24:58 +0800

    Slug to Description

--- src/views/skills/SkillCreate.vue
+++ src/views/skills/SkillCreate.vue
@@ -29,7 +29,7 @@
 
             </div>
             <div class="mb-6">
-                <label for="slug" class="block mb-2 text-sm font-medium text-gray-900">Slug</label>
+                <label for="slug" class="block mb-2 text-sm font-medium text-gray-900">Description</label>
                 <input v-model="form.slug" type="text" id="slug" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
 
                 <div v-if="errors.slug">
